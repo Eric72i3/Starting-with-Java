7e609a9328f3e937d6bc94191220b422e109d8b7
Eric Möller <eric_moeller@icloud.com>
2026-02-19 12:07:06 +0000

    gute frage was ich hier mache

---
+++ substrahiere.java
@@ -0,0 +1,10 @@
+public class substrahiere {
+    public static int subtraktion(int zahl) {
+        return zahl - 2;
+    
+}
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("Subtrahiert um 2 von " + n + " ist " + subtraktion(n));
+    }
+}
